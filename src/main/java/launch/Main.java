7de5aed80b6607419eef1377d3416caa83021072
Janapamula Subrahmanyam <subbu.janapamula@gmail.com>
2020-01-03 22:04:35 +0530

    Mavenized openxava template for postgres

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,16 +1,11 @@
 package launch;
 
 import org.apache.catalina.startup.Tomcat;
-
 import java.io.File;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.hsqldb.server.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        start("RestaurantDB");
         run("Restaurant");
     }
 
@@ -26,17 +21,5 @@ public class Main {
         tomcat.start();
         System.out.println("application_started_go http://localhost:8080/" + app);
         tomcat.getServer().await();
-    }
-
-    public static void start(String dbName) throws Exception {
-        (new Thread(() -> {
-            Server hsqlServer = new Server();
-            hsqlServer.setSilent(true);
-            hsqlServer.setDatabaseName(0, "");
-            hsqlServer.setDatabasePath(0, "file:data/" + dbName);
-            hsqlServer.setPort(1666);
-            hsqlServer.start();
-            Logger.getLogger("").setLevel(Level.INFO);
-        })).start();
     }
 }

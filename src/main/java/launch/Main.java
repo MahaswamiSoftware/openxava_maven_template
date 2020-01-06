8bcb01fd92c1325ab0006dda3ec5e16325a8817b
Janapamula Subrahmanyam <subbu.janapamula@gmail.com>
2020-01-06 13:06:40 +0530

    Openxava maven template custom port number specification implemented

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -5,21 +5,31 @@ import java.io.File;
 
 public class Main {
 
+    private static final int PORT_NUMBER = 8080;
+
     public static void main(String[] args) throws Exception {
-        run("Restaurant");
+        int port  = PORT_NUMBER;
+        if(args.length > 0 && args[0].length() == 4) {
+            try {
+                port = Integer.parseInt(args[0]);
+            }catch (NumberFormatException nm){
+                port = PORT_NUMBER;
+            }
+        }
+        run("Restaurant", port);
     }
 
-    public static void run(String app) throws Exception {
+    public static void run(String app, int port) throws Exception {
         System.out.println("starting_application");
         String webappDir = (new File("web")).getAbsolutePath();
         Tomcat tomcat = new Tomcat();
         tomcat.setBaseDir("temp");
-        tomcat.setPort(8080);
+        tomcat.setPort(port);
         tomcat.getConnector();
         tomcat.enableNaming();
         tomcat.addWebapp("/" + app, webappDir);
         tomcat.start();
-        System.out.println("application_started_go http://localhost:8080/" + app);
+        System.out.println("application_started_go http://localhost:" + port + "/" + app);
         tomcat.getServer().await();
     }
 }

5ae010c5fbc336f00ff17068355f50183f6ac859
Janapamula Subrahmanyam <subbu.janapamula@gmail.com>
2020-01-13 11:22:18 +0530

    Modifying the openxava template hold the generated classes

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,7 +1,17 @@
 package launch;
 
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.WebResourceSet;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.EmptyResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
 import java.io.File;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Main {
 
@@ -21,15 +31,60 @@ public class Main {
 
     public static void run(String app, int port) throws Exception {
         System.out.println("starting_application");
-        String webappDir = (new File("web")).getAbsolutePath();
+
+        File root = getRootFolder();
+        System.setProperty("org.apache.catalina.startup.EXIT_ON_INIT_FAILURE", "true");
         Tomcat tomcat = new Tomcat();
-        tomcat.setBaseDir("temp");
+        Path tempPath = Files.createTempDirectory("tomcat-base-dir");
+        tomcat.setBaseDir(tempPath.toString());
         tomcat.setPort(port);
         tomcat.getConnector();
         tomcat.enableNaming();
-        tomcat.addWebapp("/" + app, webappDir);
+
+        File webContentFolder = new File(root.getAbsolutePath(), "web");
+        if (!webContentFolder.exists()) {
+            webContentFolder = Files.createTempDirectory("default-doc-base").toFile();
+        }
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/" + app, webContentFolder.getAbsolutePath());
+        //Set execution independent of current thread context classloader (compatibility with exec:java mojo)
+        ctx.setParentClassLoader(Main.class.getClassLoader());
+
+        System.out.println("configuring app with basedir: " + webContentFolder.getAbsolutePath());
+
+        // Declare an alternative location for your "WEB-INF/classes" dir
+        // Servlet 3.0 annotation will work
+        File additionWebInfClassesFolder = new File(root.getAbsolutePath(), "target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+
+        WebResourceSet resourceSet;
+        if (additionWebInfClassesFolder.exists()) {
+            resourceSet = new DirResourceSet(resources, "/WEB-INF/classes", additionWebInfClassesFolder.getAbsolutePath(), "/");
+            System.out.println("loading WEB-INF resources from as '" + additionWebInfClassesFolder.getAbsolutePath() + "'");
+        } else {
+            resourceSet = new EmptyResourceSet(resources);
+        }
+        resources.addPreResources(resourceSet);
+        ctx.setResources(resources);
+
         tomcat.start();
         System.out.println("application_started_go http://localhost:" + port + "/" + app);
         tomcat.getServer().await();
+    }
+
+    private static File getRootFolder() {
+        try {
+            File root;
+            String runningJarPath = Main.class.getProtectionDomain().getCodeSource().getLocation().toURI().getPath().replaceAll("\\\\", "/");
+            int lastIndexOf = runningJarPath.lastIndexOf("/target/");
+            if (lastIndexOf < 0) {
+                root = new File("");
+            } else {
+                root = new File(runningJarPath.substring(0, lastIndexOf));
+            }
+            System.out.println("application resolved root folder: " + root.getAbsolutePath());
+            return root;
+        } catch (URISyntaxException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
